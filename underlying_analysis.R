4a262223c6ad31c2d3920fb5113efaa70997e50d
r4dat <rfourtoss@gmail.com>
2014-07-15 16:55:35 -0500

    Start underlying analysis prior to Markup.

---
+++ underlying_analysis.R
@@ -0,0 +1,29 @@
+library(lubridate)
+library(dplyr)
+library(stringr)
+# Reproducible Research: Peer Assessment 1
+
+
+## Loading and preprocessing the data
+act = read.csv(file = unzip("activity.zip"),header = TRUE,stringsAsFactors=FALSE)
+
+## Pad "interval" into mil-time format. 
+act$interval=str_pad(string = act$interval,width = 4,side = "left",pad = 0)
+
+## Create combined date-time column and parse with lubridate.
+act = mutate(.data = act,datetime=ymd_hm(paste(act$date,act$interval)))
+
+
+## What is mean total number of steps taken per day?
+
+
+
+## What is the average daily activity pattern?
+
+
+
+## Imputing missing values
+
+
+
+## Are there differences in activity patterns between weekdays and weekends?
