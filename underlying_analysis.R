010bae0a85ac23e843fc0853648200fba9e3c154
r4dat <rfourtoss@gmail.com>
2014-07-16 10:40:23 -0500

    Add group by, summation by day, histogram, and numerical summary.

--- underlying_analysis.R
+++ underlying_analysis.R
@@ -12,10 +12,12 @@ act$interval=str_pad(string = act$interval,width = 4,side = "left",pad = 0)
 
 ## Create combined date-time column and parse with lubridate.
 act = mutate(.data = act,datetime=ymd_hm(paste(act$date,act$interval)))
-
+group_date = group_by(act,date)
+summ = summarize(by_day,tot_steps=sum(steps,na.rm = FALSE))
+ggplot(summ, aes(x=tot_steps))+geom_histogram(colour="white")+ylab("Frequency Count")+xlab("Total Steps Per Day")
 
 ## What is mean total number of steps taken per day?
-
+summary(summ$tot_steps)
 
 
 ## What is the average daily activity pattern?
